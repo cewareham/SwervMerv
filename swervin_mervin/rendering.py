3ceca8eca6eee867ab401c7cbca95bf08895e5e4
cewareham <cwareham007@gmail.com>
2020-12-12 23:50:42 -0700

    Step 24 Got road rendering right way up

--- swervin_mervin/rendering.py
+++ swervin_mervin/rendering.py
@@ -1,13 +1,13 @@
 # Helper functions for rendering.
 
 def segment_pointlist(segment):
-    bottom = segment["bottom"]["screen"]
     top    = segment["top"]["screen"]
+    bottom = segment["bottom"]["screen"]
 
-    return [((bottom["x"] - bottom["w"]), bottom["y"]),
-            ((bottom["x"] + bottom["w"]), bottom["y"]),
-            ((top["x"] + top["w"]), top["y"]),
-            ((top["x"] - top["w"]), top["y"])]
+    return [((bottom["x"] - bottom["w"]), (480 - bottom["y"])),
+            ((bottom["x"] + bottom["w"]), (480 - bottom["y"])),
+            ((top["x"] + top["w"]), (480 - top["y"])),
+            ((top["x"] - top["w"]), (480 - top["y"]))]
 
 def render_player():
     pass
