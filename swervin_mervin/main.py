738d354062ff36501d4c998a38eab13f02d48ad4
cewareham <cwareham007@gmail.com>
2020-12-14 23:49:43 -0700

    Step 034 Fixed looping issue (added rumble & lines)

--- swervin_mervin/main.py
+++ swervin_mervin/main.py
@@ -22,7 +22,7 @@ road_width     = 1500
 top_speed      = (segment_height / (1.0/fps))
 acceleration   = top_speed / 9.0
 field_of_view  = 100 # Degrees
-camera_height  = 1000
+camera_height  = 1400
 camera_depth   = 1 / math.tan((field_of_view / 2) * math.pi / 180);
 player_x       = 0
 direction_x    = 0
@@ -30,10 +30,12 @@ player_z       = camera_height * camera_depth
 colours        = {"white": pygame.Color(255, 255, 255),
                   "light": {"road": pygame.Color(193, 193, 193),
                             "grass": pygame.Color(61, 212, 76),
-                            "lane": pygame.Color(255, 255, 255)},
+                            "rumble": pygame.Color(223, 215, 1),
+                            "line": pygame.Color(255, 255, 255)},
                   "dark": {"road": pygame.Color(173, 173, 173),
                            "grass": pygame.Color(50, 186, 62),
-                           "lane": pygame.Color(255, 255, 255)}}
+                           "rumble": pygame.Color(192, 186, 0),
+                           "line": pygame.Color(255, 255, 255)}}
 
 segments       = build_segments(segment_height, rumble_length, colours)
 track_length   = len(segments) * segment_height
@@ -60,18 +62,24 @@ while True:
     base_segment = find_segment(position, segments, segment_height)
 
     for s in range(draw_distance):
-        index             = (base_segment["index"] + s) % len(segments)
-        segment           = segments[index]
+        index              = (base_segment["index"] + s) % len(segments)
+        segment            = segments[index]
+        projected_position = position
 
-        project_line(segment, "top", (player_x * road_width), camera_height, position, camera_depth, dimensions, road_width)
-        project_line(segment, "bottom", (player_x * road_width), camera_height, position, camera_depth, dimensions, road_width)
+        # Past end of track and looped back.
+        if segment["index"] < base_segment["index"]:
+            projected_position -= track_length
+
+        project_line(segment, "top", (player_x * road_width), camera_height, projected_position, camera_depth, dimensions, road_width)
+        project_line(segment, "bottom", (player_x * road_width), camera_height, projected_position, camera_depth, dimensions, road_width)
 
         # Segment is behind us.
         if segment["bottom"]["camera"]["z"] <= camera_depth:
             continue
 
         render_grass(window, segment, dimensions)
-        render_road(window, segment, dimensions)
+        render_road(window, segment, dimensions, rumble_length)
+        render_player(window, segment, dimensions)
 
     for event in pygame.event.get():
         if event.type == QUIT:
--- swervin_mervin/rendering.py
+++ swervin_mervin/rendering.py
@@ -2,15 +2,46 @@
 
 import pygame
 
-def render_road(window, segment, dimensions):
+def render_road(window, segment, dimensions, rumble_length):
     top    = segment["top"]["screen"]
     bottom = segment["bottom"]["screen"]
+
+    # Road.
     points = [((bottom["x"] - bottom["w"]), (dimensions[1] - bottom["y"])),
               ((bottom["x"] + bottom["w"]), (dimensions[1] - bottom["y"])),
               ((top["x"] + top["w"]), (dimensions[1] - top["y"])),
               ((top["x"] - top["w"]), (dimensions[1] - top["y"]))]
-
     pygame.draw.polygon(window, segment["colour"]["road"], points)
+
+    # Rumble strip.
+    left_rumble_width  = top["w"] / 6
+    right_rumble_width = bottom["w"] / 6
+
+    points = [((bottom["x"] - bottom["w"] - left_rumble_width), (dimensions[1] - bottom["y"])),
+              ((bottom["x"] - bottom["w"]), (dimensions[1] - bottom["y"])),
+              ((top["x"] - top["w"]), (dimensions[1] - top["y"])),
+              ((top["x"] - top["w"] - left_rumble_width), (dimensions[1] - top["y"]))]
+
+    pygame.draw.polygon(window, segment["colour"]["rumble"], points)
+
+    points = [((bottom["x"] + bottom["w"] + right_rumble_width), (dimensions[1] - bottom["y"])),
+              ((bottom["x"] + bottom["w"]), (dimensions[1] - bottom["y"])),
+              ((top["x"] + top["w"]), (dimensions[1] - top["y"])),
+              ((top["x"] + top["w"] + right_rumble_width), (dimensions[1] - top["y"]))]
+
+    pygame.draw.polygon(window, segment["colour"]["rumble"], points)
+
+    if (segment["index"] / rumble_length) % 2 == 0:
+        bottom_line_width = (bottom["w"] / 32)
+        top_line_width    = (top["w"] / 32)
+        
+        points = [(bottom["x"], (dimensions[1] - bottom["y"])),
+                  ((bottom["x"] + bottom_line_width * 2), (dimensions[1] - bottom["y"])),
+                  ((top["x"] + top_line_width * 2), (dimensions[1] - top["y"])),
+                  (top["x"], (dimensions[1] - top["y"]))]
+                  
+        pygame.draw.polygon(window, segment["colour"]["line"], points)
+
 
 def render_grass(window, segment, dimensions):
     top       = segment["top"]["screen"]
@@ -23,5 +54,5 @@ def render_grass(window, segment, dimensions):
     else:
         pygame.draw.rect(window, segment["colour"]["grass"], (0, y, dimensions[0], height))
 
-def render_player():
+def render_player(window, segment, dimensions):
     pass
